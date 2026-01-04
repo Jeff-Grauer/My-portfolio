ed01c05952cac74cbccbecb1fa416eca9282b618
Jeff-Grauer <jeffgrauer1@outlook.com>
2026-01-03 21:55:25 -0500

    Resolve merge conflicts

--- src/data/education.ts
+++ src/data/education.ts
@@ -4,7 +4,7 @@ export const EDUCATION: ExperienceShowcaseListItemProps[] = [
   {
     title: "Master's degree, Software engineering",
     organisation: {
-      name: "Tulsa Technology Center",
+      name: "Tulsa University Of Technology",
       href: "https://tulsatech.edu/",
     },
     date: "09/2015 - 05/2016",
@@ -14,31 +14,12 @@ export const EDUCATION: ExperienceShowcaseListItemProps[] = [
   {
     title: "Bachelor's degree, Computer science",
     organisation: {
-      name: "Tulsa Technology Center",
+      name: "Tulsa University Of Technology",
       href: "https://tulsatech.edu/",
     },
     date: "2006 - 2010",
     location: "Tulsa, United States",
     description: "",
   },
-  //{
-  //title: "Bachelor in Software Engineering and Information System",
-  //organisation: {
-  //name: "ISIMM | Higher Institute of Informatics and Mathematics of Monastir",
-  //href: "http://www.isimm.rnu.tn/public/",
-  //},
-  //date: "2021-2024",
-  //location: "Monastir - Tunisie",
-  //description: "",
-  // },
-  //{
-  //title: "Baccalaureate in Computer Science",
-  //organisation: {
-  //name: "Bekalta Secondary School",
-  //href: "#",
-  //},
-  //date: "2020-2021",
-  //location: "Bekalta - Monastir - Tunisie",
-  // description: "",
-  // },
 ];
+
